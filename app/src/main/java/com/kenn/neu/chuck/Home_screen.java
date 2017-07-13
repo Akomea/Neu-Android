0954f07699ddd8f393118fee23220e2f1697b936
Kenn <kenn72@outlook.com>
2017-07-13 11:11:25 +0800

    Implement custom query with name as params

--- app/src/main/java/com/kenn/neu/chuck/Home_screen.java
+++ app/src/main/java/com/kenn/neu/chuck/Home_screen.java
@@ -1,10 +1,12 @@
 package com.kenn.neu.chuck;
 
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -12,7 +14,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Home_screen extends AppCompatActivity {
@@ -20,9 +21,10 @@ public class Home_screen extends AppCompatActivity {
     private TextView textView;
     private Button button;
     private ProgressBar pbLoading;
+    private EditText edtFirstName;
+    private EditText edtLastName;
+
     private static final String CHUCK_QUERY = "http://api.icndb.com/jokes/random?limitTo=[nerdy]";
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,20 +40,27 @@ public class Home_screen extends AppCompatActivity {
             }
         });
         pbLoading = (ProgressBar) findViewById(R.id.pb_loading);
+        edtFirstName= (EditText) findViewById(R.id.fname);
+        edtLastName= (EditText) findViewById(R.id.lname);
     }
 
     private void loadChuckQuotes() {
-        new ChuckQuoteTask().execute(CHUCK_QUERY);
+        //new ChuckQuoteTask().execute(CHUCK_QUERY);
+        String firstName= edtFirstName.getText().toString();
+        String lastName= edtLastName.getText().toString();
+        URL url = NetworkUtils.buildUrl(firstName, lastName);
+        new ChuckQuoteTask().execute(url);
+
+
 
     }
-
     /**
      * This class extends AsyncTask to execute the query out of the main thread.
      * We should always not run a time consuming task on main thread.
      */
-    private class ChuckQuoteTask extends AsyncTask<String, Object, String> {
+    private class ChuckQuoteTask extends AsyncTask<URL, Object, String> {
 
-        @Override
+            @Override
         protected void onPreExecute() {
             // Here we prepare what we need to be done on the UI
             // For example, show a progress bar for a download task.
@@ -60,18 +69,23 @@ public class Home_screen extends AppCompatActivity {
         }
 
         @Override
-        protected String doInBackground(String... params) {
-            String result = null;
+        protected String doInBackground(URL... params) {
 
-            try {
-                URL url = new URL(params[0]);
-                result = NetworkUtils.getResponseFromHttpUrl(url);
-            } catch (IOException e) {
-                e.printStackTrace();
+
+                String result = null;
+
+                try {
+                    //URL url = new URL(params[0]);
+                    URL url = params[0];
+
+                    result = NetworkUtils.getResponseFromHttpUrl(url);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                return result;
             }
 
-            return result;
-        }
 
         @Override
         protected void onPostExecute(String result) {
@@ -82,15 +96,18 @@ public class Home_screen extends AppCompatActivity {
 
 
             // This function is executed on main thread as well.
-            try {
-                String joke = extractJokeFromJson(result);
-                textView.setText(joke);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            } catch (NullPointerException e) {
-                e.printStackTrace();
+            if (!TextUtils.isEmpty(result)) {
+                try {
+                    String joke = extractJokeFromJson(result);
+                    textView.setText(joke);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
+                }
+                pbLoading.setVisibility(View.GONE);
             }
-            pbLoading.setVisibility(View.GONE);
+
         }
 
         private String extractJokeFromJson(String json) throws JSONException, NullPointerException {
@@ -99,6 +116,8 @@ public class Home_screen extends AppCompatActivity {
             String joke = value.optString("joke");
             return joke;
         }
+
+
     }
 
 }
