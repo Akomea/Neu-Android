fd019d0355aeb3e6d7de08bab703f9c64baf08ab
Kenn <kenn72@outlook.com>
2017-07-14 17:24:56 +0800

    add new activity and full screen layout

---
+++ xkcd/src/main/java/com/kenn/neu/xkcd/FullScreenDetail.java
@@ -0,0 +1,13 @@
+package com.kenn.neu.xkcd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FullScreenDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fullscreen);
+    }
+}
